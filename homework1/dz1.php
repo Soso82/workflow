ab7cfa3cea79c7d77e1e4181ef4c0ad152751004
Gabaraev_SZ <soso82@mail.ru>
2017-07-17 14:12:26 +0400

    Домашка

--- homework1/dz1.php
+++
@@ -1,8 +0,0 @@
-<?php
-//ДЗ1
-$name = "Soslan";
-$age = 34;
-echo "Меня зовут .$name";
-echo "Мне .$age .года";
-echo "\"!|V"\"";
-?>
--- index.php
+++ index.php
@@ -1,9 +1,17 @@
+
+<!DOCTYPE>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+
+</head>
+<body>
+<h1> ДЗ1</h1>
 <?php
-$asd = 45;
-echo $asd;
-mhbkjb
-jbmnbmnbkb
-mbjhjhgh
-
-
-
+$name = "Сослан";
+$age = 34;
+echo "Меня зовут $name \n";
+echo "Мне $age года \n";
+?>
+</body>
+</html>
